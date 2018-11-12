5352554f13357eaa67de5df9338b84aa3a983465
F1yBear <18717346409@qq.com>
2018-11-12 22:15:10 +0800

    Add files via upload

---
+++ src/main/java/study/easycode/chapter1/JavaType.java
@@ -0,0 +1,23 @@
+package study.easycode.chapter1;
+
+
+public class JavaType {
+
+    Byte aByte;
+    Character character;
+    Integer integer;
+    Long aLong;
+    Float aFloat;
+    Double aDouble;
+    Boolean aBoolean;
+    String string;
+
+
+    public static void main(String[] args) {
+        Character i = 'j';
+
+        Integer integer = new Integer(10);
+        System.out.print(i.charValue());
+
+    }
+}
